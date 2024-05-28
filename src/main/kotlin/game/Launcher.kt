9823b513410c06d928b051b9d4f972a53ea69dae
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-05-27 23:55:54 -0400

    Initial commit

---
+++ src/main/kotlin/game/Launcher.kt
@@ -0,0 +1,11 @@
+package game
+
+import org.samaelheaven.zenith.core.App
+
+class Launcher {
+    companion object {
+        @JvmStatic fun main(args : Array<String>) {
+            App.launch()
+        }
+    }
+}
