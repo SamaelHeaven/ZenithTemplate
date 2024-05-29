89ec094c83868f1b0406637c0568241e8d2d53ac
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-05-28 23:28:51 -0400

    Updated icon.png

--- src/main/kotlin/game/Launcher.kt
+++ src/main/kotlin/game/Launcher.kt
@@ -2,14 +2,14 @@ package game
 
 import org.samaelheaven.zenith.core.Game
 import org.samaelheaven.zenith.core.GameConfig
-import org.samaelheaven.zenith.utils.url
+import org.samaelheaven.zenith.utils.Resource
 
 class Launcher {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
             val config = GameConfig(
-                icon = url("icon.png")
+                icon = Resource.url("icon.png")
             )
             Game.launch(MainScene(), config)
         }
