e4fe66468cf9de9fdabefae81a7be22af5d72cb8
SamaelHeaven <samuelparadisinfo@gmail.com>
2024-05-29 12:38:52 -0400

    Refactor: Minor changes

--- src/main/kotlin/game/Launcher.kt
+++ src/main/kotlin/game/Launcher.kt
@@ -4,14 +4,12 @@ import org.samaelheaven.zenith.core.Game
 import org.samaelheaven.zenith.core.GameConfig
 import org.samaelheaven.zenith.utils.Resource
 
-class Launcher {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            val config = GameConfig(
-                icon = Resource.url("icon.png")
-            )
-            Game.launch(MainScene(), config)
-        }
+object Launcher {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val config = GameConfig(
+            icon = Resource.url("icon.png")
+        )
+        Game.launch(MainScene(), config)
     }
 }
