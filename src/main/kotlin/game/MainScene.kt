fce12ff44840c6936d4455e3a20d3a24d93dfeca
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-05-30 16:03:05 -0400

    Fullscreen toggle

--- src/main/kotlin/game/MainScene.kt
+++ src/main/kotlin/game/MainScene.kt
@@ -1,7 +1,10 @@
 package game
 
+import zenith.core.Game
 import zenith.core.Renderer
 import zenith.core.Scene
+import zenith.input.Key
+import zenith.input.Keyboard
 import zenith.paint.Color
 
 class MainScene : Scene() {
@@ -12,5 +15,8 @@ class MainScene : Scene() {
     override fun update() {
         println("Updating")
         Renderer.clearBackground(Color.BLUE)
+        if (Keyboard.isKeyPressed(Key.TAB)) {
+            Game.fullscreen = !Game.fullscreen
+        }
     }
 }
