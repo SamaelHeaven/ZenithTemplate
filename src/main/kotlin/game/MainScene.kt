8342e90b399c97af868d36da860bee114987df30
SamaelHeaven <samuelparadisinfo@gmail.com>
2024-06-03 15:54:15 -0400

    Moving mouse

--- src/main/kotlin/game/MainScene.kt
+++ src/main/kotlin/game/MainScene.kt
@@ -3,8 +3,11 @@ package game
 import zenith.core.Game
 import zenith.core.Renderer
 import zenith.core.Scene
+import zenith.core.Time
 import zenith.input.Key
 import zenith.input.Keyboard
+import zenith.input.Mouse
+import zenith.math.Vector2
 import zenith.paint.Color
 
 class MainScene : Scene() {
@@ -20,6 +23,19 @@ class MainScene : Scene() {
         }
         if (Keyboard.isKeyPressed(Key.ESCAPE)) {
             Game.exit()
+        }
+        val cursorSpeed = 300 * Time.delta
+        if (Keyboard.isKeyDown(Key.UP)) {
+            Mouse.position += Vector2.UP * cursorSpeed
+        }
+        if (Keyboard.isKeyDown(Key.DOWN)) {
+            Mouse.position += Vector2.DOWN * cursorSpeed
+        }
+        if (Keyboard.isKeyDown(Key.LEFT)) {
+            Mouse.position += Vector2.LEFT * cursorSpeed
+        }
+        if (Keyboard.isKeyDown(Key.RIGHT)) {
+            Mouse.position += Vector2.RIGHT * cursorSpeed
         }
     }
 }
