b67a5604da846409424ffc7439055c5465670ffe
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-06-06 02:14:50 -0400

    Refactor: Minor changes

--- src/main/kotlin/game/MyPrefab.kt
+++ src/main/kotlin/game/MyPrefab.kt
@@ -4,6 +4,10 @@ import zenith.core.Entity
 import zenith.core.Prefab
 
 class MyPrefab : Prefab() {
+    override var name: String
+        get() = "MyEntity"
+        set(_) {}
+
     override fun build(entity: Entity) {
         entity.addComponent(MyComponent())
     }
