050a6a5b446767b618b825ceb4e9cbf6efd3ab3c
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-06-18 16:54:46 -0400

    WIP bugfix

--- src/main/kotlin/game/MainScene.kt
+++ src/main/kotlin/game/MainScene.kt
@@ -24,5 +24,6 @@ class MainScene : Scene() {
         camera.zoom += Mouse.scroll.y * 0.005f
         camera.zoom -= round(Gamepad[0].getAxis(GamepadAxis.RIGHT_Y)) * 2 * Time.delta
         camera.zoom = 0.01f.coerceAtLeast(camera.zoom)
+        println(Time.currentFPS)
     }
 }
